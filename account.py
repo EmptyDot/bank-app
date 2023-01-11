970d821c9b34ba207bf2abbc2528addaa598b27c
Edvin Hermansson <edvin.hermanson@gmail.com>
2023-01-11 23:28:04 +0100

    Make balance private and not protected

--- account.py
+++ account.py
@@ -7,7 +7,7 @@ getcontext()
 class Account:
     def __init__(self, account_number: int, balance: int | float = 0):
         self.account_number = account_number
-        self._balance = Decimal(balance)
+        self.__balance = Decimal(balance)
 
     @property
     def balance(self) -> float:
@@ -15,7 +15,7 @@ class Account:
         Get the balance to be presented to the user, not to be used for arithmetic operations.
         :return: Balance represented as a float with 2 decimals of precision
         """
-        return float(round(self._balance, 2))
+        return float(round(self.__balance, 2))
 
     def balance_add(self, amount: int | float) -> bool:
         """
@@ -26,7 +26,7 @@ class Account:
         if not isinstance(amount, (int, float)) or amount <= 0:
             return False
 
-        self._balance = self._balance + Decimal(amount)
+        self.__balance = self.__balance + Decimal(amount)
         return True
 
     def balance_sub(self, amount: int | float) -> bool:
@@ -35,15 +35,19 @@ class Account:
         :param amount: Amount to be subtracted
         :return: True if successful else False
         """
-        if not isinstance(amount, (int, float)) or amount <= 0 or amount > self._balance:
+        if not isinstance(amount, (int, float)) or amount <= 0 or amount > self.__balance:
             return False
 
-        self._balance = self._balance - Decimal(amount)
+        self.__balance = self.__balance - Decimal(amount)
         return True
 
+    def __eq__(self, other_account: Account):
+        return self.account_number == other_account.account_number \
+            and self.balance == other_account.balance
+
     def __str__(self):
-        return f"Account({self.account_number}, balance={self._balance})"
+        return f"Account({self.account_number}, balance={self.balance})"
 
     def __repr__(self):
-        return f"Account({self.account_number}, balance={self._balance})"
+        return f"Account({self.account_number}, balance={self.balance})"
 
