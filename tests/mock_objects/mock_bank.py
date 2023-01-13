c1700138a2d0e7b160b23dd07aa8e536146838d2
Edvin Hermansson <edvin.hermanson@gmail.com>
2023-01-13 20:39:16 +0100

    Tests using mock objects #29

--- tests/mock_objects/mock_bank.py
+++ tests/mock_objects/mock_bank.py
@@ -2,13 +2,10 @@ from typing import Optional
 
 from bank import Bank
 from customer import Customer
-from mock_parser import MockParser
+from tests.mock_objects.mock_parser import MockParser
 
 
 class MockBank(Bank):
     def __init__(self, customers: list[Customer], save_on_exit: bool):
         super().__init__(save_on_exit=save_on_exit, parser=MockParser())
         self.customers = customers
-        self.current_user: Optional[Customer] = None
-
-
--- tests/test_bank.py
+++ tests/test_bank.py
@@ -2,24 +2,29 @@ from bank import Bank
 
 from customer import Customer
 from account import Account
-from mock_bank import MockBank
+from tests.mock_objects.mock_bank import MockBank
+from tests.mock_objects.mock_parser import MockParser
 
+
+def get_bank(customers: list[Customer] = None):
+    customers = customers if customers else []
+    return MockBank(customers, save_on_exit=False)
 
 class TestBank:
     def test_get_customers(self):
         customers = [Customer("Bob", "123"), Customer("Alice", "456")]
-        bank = MockBank(customers, save_on_exit=False)
+        bank = get_bank(customers)
         assert bank.get_customers() == customers
 
     def test_add_customer(self):
-        bank = Bank(save_on_exit=False)
+        bank = get_bank()
         name, password = "Bob", "123"
         assert bank.add_customer(name, password)
         assert len(bank.customers) == 1
         assert bank.customers[-1] == Customer(name, password)
 
     def test_add_customer_non_unique(self):
-        bank = Bank(save_on_exit=False)
+        bank = get_bank()
         name, password = "Bob", "123"
         c = Customer(name, password)
         assert bank.add_customer(name, password)
@@ -28,33 +33,36 @@ class TestBank:
         assert bank.customers[-1] == c
 
     def test_add_customer_wrong_type(self):
-        bank = Bank(save_on_exit=False)
+        bank = get_bank()
         name, password = ["bob"], 123
         assert bank.add_customer(name, password) is False
         assert len(bank.customers) == 0
 
     def test_get_customer(self):
-        bank = MockBank([Customer("Bob", "123"), Customer("Alice", "456")], save_on_exit=False)
+        bank = get_bank([Customer("Bob", "123"), Customer("Alice", "456")])
+
+
+
         c = bank.get_customer("Bob")
         bob = Customer("Bob", "123")
         assert c is not None
         assert bob == c
 
     def test_get_customer_fail(self):
-        bank = Bank(save_on_exit=False)
+        bank = get_bank()
         assert bank.get_customer("Bob") is None
 
     def test_change_customer_password(self):
-        bank = MockBank([Customer("Bob", "123"), Customer("Alice", "456")], save_on_exit=False)
+        bank = get_bank([Customer("Bob", "123"), Customer("Alice", "456")])
         assert bank.change_customer_password("Bob", "789")
         assert bank.customers[0].password == "789"
 
     def test_change_customer_password_fail(self):
-        bank = MockBank([Customer("Bob", "123")], save_on_exit=False)
+        bank = get_bank([Customer("Bob", "123")])
         assert bank.change_customer_password("Alice", "789") is False
 
     def test_remove_customer(self):
-        bank = MockBank([Customer("Bob", "123"), Customer("Alice", "456")], save_on_exit=False)
+        bank = get_bank([Customer("Bob", "123"), Customer("Alice", "456")])
         bob = bank.customers[0]
         assert bob in bank.customers
         assert bank.remove_customer("Bob")
@@ -62,12 +70,12 @@ class TestBank:
         assert bob not in bank.customers
 
     def test_remove_customer_fail(self):
-        bank = MockBank([Customer("Bob", "123")], save_on_exit=False)
+        bank = get_bank([Customer("Bob", "123")])
         assert bank.remove_customer("Alice") is False
         assert len(bank.customers) == 1
 
     def test_remove_customer_logged_in(self):
-        bank = MockBank([Customer("Bob", "123"), Customer("Alice", "456")], save_on_exit=False)
+        bank = get_bank([Customer("Bob", "123"), Customer("Alice", "456")])
         bob = bank.customers[0]
         bank.current_user = bob
         assert bob in bank.customers
@@ -77,82 +85,82 @@ class TestBank:
         assert bank.current_user is None
 
     def test_login(self):
-        bank = MockBank([Customer("Bob", "123")], save_on_exit=False)
+        bank = get_bank([Customer("Bob", "123")])
         assert bank.login("Bob", "123")
         assert bank.current_user == Customer("Bob", "123")
 
     def test_login_wrong_password(self):
-        bank = MockBank([Customer("Bob", "123")], save_on_exit=False)
+        bank = get_bank([Customer("Bob", "123")])
         assert bank.login("Bob", "bad_password") is False
         assert bank.current_user is None
 
     def test_login_wrong_name(self):
-        bank = MockBank([Customer("Bob", "123")], save_on_exit=False)
+        bank = get_bank([Customer("Bob", "123")])
         assert not bank.login("Alice", "123")
         assert bank.current_user is None
 
     def test_logout(self):
-        bank = Bank(save_on_exit=False)
+        bank = get_bank()
         bank.current_user = Customer("Bob", "123")
         assert bank.logout()
         assert bank.current_user is None
 
     def test_logout_fail(self):
-        bank = Bank(save_on_exit=False)
+        bank = get_bank()
         assert bank.logout() is False
 
     def test_get_accounts(self):
-        bank = Bank(save_on_exit=False)
+        bank = get_bank()
         bank.current_user = Customer("Bob", "123")
         assert type(bank.get_accounts()) == list
 
     def test_get_accounts_fail(self):
-        bank = Bank(save_on_exit=False)
+        bank = get_bank()
         assert bank.get_accounts() is None
 
     def test_add_account(self):
-        bank = Bank(save_on_exit=False)
+        bank = get_bank()
         bank.current_user = Customer("Bob", "123")
         assert bank.add_account(1101000)
         assert bank.add_account(1101001)
         assert len(bank.current_user.accounts) == 2
 
     def test_add_account_no_user(self):
-        bank = Bank(save_on_exit=False)
+        bank = get_bank()
         assert bank.add_account(1101000) is False
 
     def test_add_account_non_unique(self):
-        bank = Bank(save_on_exit=False)
+        bank = get_bank()
         bank.current_user = Customer("Bob", "123")
         assert bank.add_account(1)
         assert bank.add_account(1) is False
         assert len(bank.current_user.accounts) == 1
 
     def test_add_account_wrong_type(self):
-        bank = Bank(save_on_exit=False)
+        bank = get_bank()
         bank.current_user = Customer("Bob", "123")
         assert bank.add_account("1101000") is False
         assert len(bank.current_user.accounts) == 0
 
     def test_remove_account(self):
-        bank = Bank(save_on_exit=False)
+        bank = get_bank()
         bank.current_user = Customer("Bob", "123")
         bank.current_user.accounts = [Account(1), Account(2)]
         assert bank.remove_account(1)
         assert len(bank.current_user.accounts) == 1
 
     def test_remove_account_no_user(self):
-        bank = Bank(save_on_exit=False)
+        bank = get_bank()
         assert bank.remove_account(1) is False
 
     def test_remove_account_no_account(self):
-        bank = Bank(save_on_exit=False)
+        bank = get_bank()
         bank.current_user = Customer("Bob", "123")
         assert bank.remove_account(1) is False
         assert len(bank.current_user.accounts) == 0
 
     def test_get_account(self):
-        bank = Bank(save_on_exit=False)
+        bank = get_bank()
         bank.current_user = Customer("Bob", "123")
         bank.current_user.accounts = [Account(1), Account(2)]
         acc = bank.get_account(1)
@@ -160,112 +168,109 @@ class TestBank:
         assert acc.account_number == 1
 
     def test_get_account_no_user(self):
-        bank = Bank(save_on_exit=False)
+        bank = get_bank()
         assert bank.get_account(1) is None
 
     def test_get_account_no_accounts(self):
-        bank = Bank(save_on_exit=False)
+        bank = get_bank()
         bank.current_user = Customer("Bob", "123")
         assert bank.get_account(1) is None
 
     def test_get_account_wrong_account_number(self):
-        bank = Bank(save_on_exit=False)
+        bank = get_bank()
         bank.current_user = Customer("Bob", "123")
         bank.current_user.accounts = [Account(1), Account(2)]
         assert bank.get_account(3) is None
 
     def test_deposit_int(self):
-        bank = Bank(save_on_exit=False)
+        bank = get_bank()
         bank.current_user = Customer("Bob", "123")
         bank.current_user.accounts = [Account(1)]
         assert bank.deposit(1, 100)
         assert bank.current_user.accounts[0].balance == 100
 
     def test_deposit_float(self):
-        bank = Bank(save_on_exit=False)
+        bank = get_bank()
         bank.current_user = Customer("Bob", "123")
         bank.current_user.accounts = [Account(1)]
         assert bank.deposit(1, 1.23456789)
         assert bank.current_user.accounts[0].balance == 1.23
 
     def test_deposit_negative(self):
-        bank = Bank(save_on_exit=False)
+        bank = get_bank()
         bank.current_user = Customer("Bob", "123")
         bank.current_user.accounts = [Account(1, 100)]
         assert bank.deposit(1, -100) is False
         assert bank.current_user.accounts[0].balance == 100
 
     def test_deposit_no_user(self):
-        bank = Bank(save_on_exit=False)
+        bank = get_bank()
         assert bank.deposit(1, 100) is False
 
     def test_deposit_wrong_type(self):
-        bank = Bank(save_on_exit=False)
+        bank = get_bank()
         bank.current_user = Customer("Bob", "123")
         bank.current_user.accounts = [Account(1)]
         assert bank.deposit(1, "100") is False
         assert bank.current_user.accounts[0].balance == 0
 
     def test_deposit_no_accounts(self):
-        bank = Bank(save_on_exit=False)
+        bank = get_bank()
         bank.current_user = Customer("Bob", "123")
         assert not bank.deposit(1, 100)
 
     def test_deposit_wrong_account_number(self):
-        bank = Bank(save_on_exit=False)
+        bank = get_bank()
         bank.current_user = Customer("Bob", "123")
         bank.current_user.accounts = [Account(1), Account(2)]
         assert bank.deposit(3, 100) is False
 
     def test_withdraw_int(self):
-        bank = Bank(save_on_exit=False)
+        bank = get_bank()
         bank.current_user = Customer("Bob", "123")
         bank.current_user.accounts = [Account(1, 100)]
         assert bank.withdraw(1, 1)
         assert bank.current_user.accounts[0].balance == 99
 
     def test_withdraw_float(self):
-        bank = Bank(save_on_exit=False)
+        bank = get_bank()
         bank.current_user = Customer("Bob", "123")
         bank.current_user.accounts = [Account(1, 100)]
         assert bank.withdraw(1, 0.1)
         assert bank.current_user.accounts[0].balance == 99.9
 
     def test_withdraw_negative(self):
-        bank = Bank(save_on_exit=False)
+        bank = get_bank()
         bank.current_user = Customer("Bob", "123")
         bank.current_user.accounts = [Account(1, 100)]
         assert bank.withdraw(1, -100) is False
         assert bank.current_user.accounts[0].balance == 100
 
     def test_withdraw_negative_no_user(self):
-        bank = Bank(save_on_exit=False)
+        bank = get_bank()
         assert bank.withdraw(1, 100) is False
 
     def test_withdraw_no_accounts(self):
-        bank = Bank(save_on_exit=False)
+        bank = get_bank()
         bank.current_user = Customer("Bob", "123")
         assert bank.withdraw(1, 100) is False
 
     def test_withdraw_wrong_account_number(self):
-        bank = Bank(save_on_exit=False)
+        bank = get_bank()
         bank.current_user = Customer("Bob", "123")
         bank.current_user.accounts = [Account(1)]
         assert bank.withdraw(3, 100) is False
 
     def test_withdraw_wrong_type(self):
-        bank = Bank(save_on_exit=False)
+        bank = get_bank()
         bank.current_user = Customer("Bob", "123")
         bank.current_user.accounts = [Account(1, 100)]
         assert bank.withdraw(1, "1") is False
         assert bank.current_user.accounts[0].balance == 100
 
     def test___str__(self):
-        bank = MockBank([Customer("Bob", "123"), Customer("Alice", "456")], save_on_exit=False)
-        assert (
-            str(bank)
-            == f'Bank({[Customer("Bob", "123"), Customer("Alice", "456")]}, current_user=None)'
-        )
+        bank = get_bank([Customer("Bob", "123")])
+        assert (str(bank) == f'Bank({[Customer("Bob", "123")]}, current_user=None)')
 
     def test___str__logged_in(self):
         bank = MockBank([Customer("Bob", "123")], save_on_exit=False)
