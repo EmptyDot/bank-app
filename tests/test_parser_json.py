a21fda85b44ea9f955af93768031cabacf9c8b77
Edvin Hermansson <edvin.hermanson@gmail.com>
2023-01-18 13:15:31 +0100

    Make parser_json into loose functions

--- tests/test_parser_json.py
+++ tests/test_parser_json.py
@@ -1,7 +1,7 @@
 from bank_app.account import Account
 from bank_app.customer import Customer
-from bank_app.parser_json import CustomerParserJson
-from tests.mock_objects.mock_parser import MockParser
+from bank_app import parser_json
+from passlib.hash import bcrypt
 
 
 def get_customer_list():
@@ -15,66 +15,63 @@ def get_customer_list():
     return customers
 
 
-class TestCustomerParserJson:
-    def test_load_customers(self):
-        customers = CustomerParserJson().load_customers(
-            "tests/data/test_saved_customers_load.json"
-        )
-        assert isinstance(customers, list)
-        assert all(isinstance(customer, Customer) for customer in customers)
+def test_load_customers():
+    customers = parser_json.load_customers("tests/data/test_saved_customers_load.json")
+    assert isinstance(customers, list)
+    assert all(isinstance(customer, Customer) for customer in customers)
 
-    def test_load_customers_wrong_file(self):
-        assert (
-            CustomerParserJson().load_customers("tests/data/does_not_exist.json")
-            is None
-        )
 
-    def test_load_customers_empty_file(self):
-        assert CustomerParserJson().load_customers("tests/data/empty.json") is None
+def test_load_customers_wrong_file():
+    assert parser_json.load_customers("tests/data/does_not_exist.json") is None
 
-    def test_load_customers_os_error(self):
-        assert CustomerParserJson().load_customers("tests/data/test_isdir") is None
 
-    def test_create_customer(self):
-        name, password = "Bob", "123"
-        acc1_num, acc1_balance = 1, 200
-        acc2_num, acc2_balance = 2, 41515.24
-        accounts = [
-            {"account_number": acc1_num, "balance": acc1_balance},
-            {"account_number": acc2_num, "balance": acc2_balance},
-        ]
+def test_load_customers_empty_file():
+    assert parser_json.load_customers("tests/data/empty.json") is None
 
-        customer = CustomerParserJson().create_customer(name, password, accounts)
-        assert customer.check_name(name)
-        assert customer.check_password(password)
-        assert all(
-            customer.accounts[idx].account_number == account["account_number"]
-            and customer.accounts[idx].balance == account["balance"]
-            for idx, account in enumerate(accounts)
-        )
 
-    def test_save_customers(self):
-        customers = get_customer_list()
-        assert CustomerParserJson().save_customers(
-            customers, "tests/data/test_saved_customers_save.json"
-        )
+def test_load_customers_os_error():
+    assert parser_json.load_customers("tests/data/test_isdir") is None
 
-    def test_save_customers_no_customers(self):
-        assert (
-            CustomerParserJson().save_customers(
-                [], "tests/data/test_saved_customers_save.json"
-            )
-            is False
-        )
 
-    def test_save_customers_no_accounts(self):
-        assert CustomerParserJson().save_customers(
-            [Customer("Bob", "123")], "tests/data/test_saved_customers_save.json"
-        )
+def test_create_customer():
+    name, password = "Bob", "123"
+    acc1_num, acc1_balance = 1, 200
+    acc2_num, acc2_balance = 2, 41515.24
+    accounts = [
+        {"account_number": acc1_num, "balance": acc1_balance},
+        {"account_number": acc2_num, "balance": acc2_balance},
+    ]
+    hashed_password = bcrypt.using(rounds=13).hash(password)
+    customer = parser_json.create_customer(name, hashed_password, accounts)
+    assert customer.check_name(name)
+    assert customer.check_password(password)
+    assert all(
+        customer.accounts[idx].account_number == account["account_number"]
+        and customer.accounts[idx].balance == account["balance"]
+        for idx, account in enumerate(accounts)
+    )
 
-    def test_save_customers_os_error(self):
-        customers = get_customer_list()
-        assert (
-            CustomerParserJson().save_customers(customers, "tests/data/test_isdir")
-            is False
-        )
+
+def test_save_customers():
+    customers = get_customer_list()
+    assert parser_json.save_customers(
+        customers, "tests/data/test_saved_customers_save.json"
+    )
+
+
+def test_save_customers_no_customers():
+    assert (
+        parser_json.save_customers([], "tests/data/test_saved_customers_save.json")
+        is False
+    )
+
+
+def test_save_customers_no_accounts():
+    assert parser_json.save_customers(
+        [Customer("Bob", "123")], "tests/data/test_saved_customers_save.json"
+    )
+
+
+def test_save_customers_os_error():
+    customers = get_customer_list()
+    assert parser_json.save_customers(customers, "tests/data/test_isdir") is False
